d464bdb3b156658d3624213ba47aa8e077bf613e
goruchan <ppcfr481@yahoo.co.jp>
2022-12-04 23:43:41 +0900

    update オプション値忘れ時は現在のものを使用するように変更

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,9 +1,17 @@
 #!/usr/bin/env node
 
 var argv = require("minimist")(process.argv.slice(2));
+const today = new Date();
 
-var year = argv["y"];
-var month = argv["m"];
+var year;
+typeof argv["y"] != "boolean" && argv["y"] != undefined
+  ? (year = argv["y"])
+  : (year = today.getFullYear());
+
+var month;
+typeof argv["m"] != "boolean" && argv["m"] != undefined
+  ? (month = argv["m"])
+  : (month = today.getMonth() + 1);
 
 console.log(year);
 console.log(month);
