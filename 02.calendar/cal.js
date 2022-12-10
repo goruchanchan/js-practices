956ded0639755b505480878be6034f11b4403c65
goruchan <ppcfr481@yahoo.co.jp>
2022-12-10 11:56:42 +0900

    clean 表示方法整理

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -23,12 +23,14 @@ if (last_month_end_day != 6) {
 
 let last_date = new Date(year, month % 12, 0).getDate();
 for (let i = 1; i <= last_date; i++) {
-  if (new Date(year, month - 1, i).getDay() == 6) {
-    console.log(`${i}`.padStart(2, " "));
-  } else {
-    i == last_date
-      ? console.log(`${i}`.padStart(2, " ") + "" + " ")
-      : process.stdout.write(`${i}`.padStart(2, " ") + " ");
-  }
+  let day = new Date(year, month - 1, i).getDay();
+  let message = `${i}`.padStart(2, " ");
+
+  message = day == 6 || i == last_date ? message + "\n" : message + " ";
+  process.stdout.write(message);
 }
 console.log("");
+
+const white = "\x1b[47m";
+const reset = "\x1b[49m";
+console.log(white + "test" + reset + "hoge");
