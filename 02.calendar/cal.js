570575ae09d2b5ff797572d85aa0e899994f6ab0
goruchan <ppcfr481@yahoo.co.jp>
2022-12-10 11:28:23 +0900

    clean 英単語修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -10,24 +10,24 @@ var year =
 
 var month =
   typeof argv["m"] != "boolean" && argv["m"] != undefined
-    ? argv["m"] - 1
-    : today.getMonth();
+    ? argv["m"]
+    : today.getMonth() + 1;
 
-let last_month_end_day = new Date(year, month, 0);
-
-console.log(`      ${month + 1}月 ${year}`);
+console.log(`      ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
-if (last_month_end_day.getDay() != 6) {
-  process.stdout.write("".padEnd((last_month_end_day.getDay() + 1) * 3, " "));
+
+let last_month_end_day = new Date(year, month - 1, 0).getDay();
+if (last_month_end_day != 6) {
+  process.stdout.write("".padEnd((last_month_end_day + 1) * 3, " "));
 }
 
-let last_day = new Date(year, (month + 1) % 12, 0).getDate();
-for (let i = 1; i <= last_day; i++) {
-  if (new Date(year, month, i).getDay() == 6) {
+let last_date = new Date(year, month % 12, 0).getDate();
+for (let i = 1; i <= last_date; i++) {
+  if (new Date(year, month - 1, i).getDay() == 6) {
     console.log(`${i}`.padStart(2, " "));
   } else {
-    i == last_day
-      ? console.log(`${i}`.padStart(2, " ") + " ")
+    i == last_date
+      ? console.log(`${i}`.padStart(2, " ") + "" + " ")
       : process.stdout.write(`${i}`.padStart(2, " ") + " ");
   }
 }
