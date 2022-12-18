c43c566a511db9d40dba30c325ce5163bc00d281
goruchan <ppcfr481@yahoo.co.jp>
2022-12-18 22:51:01 +0900

    update 厳密等価演算子を使うように修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -4,8 +4,8 @@ import minimist from "minimist";
 
 const argv = minimist(process.argv.slice(2));
 const today = new Date();
-const year = argv["y"] != undefined ? argv["y"] : today.getFullYear();
-const month = argv["m"] != undefined ? argv["m"] - 1 : today.getMonth();
+const year = argv["y"] !== undefined ? argv["y"] : today.getFullYear();
+const month = argv["m"] !== undefined ? argv["m"] - 1 : today.getMonth();
 
 console.log(`      ${month + 1}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
@@ -22,7 +22,7 @@ for (let i = 1; i <= end_date; i++) {
   const day = new Date(year, month, i);
   let message = `${i}`.padStart(2, " ");
   message = isSameDay(day, today) ? `\x1b[47m${message}\x1b[49m` : message;
-  message = isSaturday(day) || i == end_date ? message + "\n" : message + " ";
+  message = isSaturday(day) || i === end_date ? message + "\n" : message + " ";
   process.stdout.write(message);
 }
 console.log("");
