2841979c3b10e9f51eef0e8823468087168d2773
goruchan <ppcfr481@yahoo.co.jp>
2023-01-11 23:47:41 +0900

    update 参照でカーソル表示に対応

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -2,6 +2,8 @@
 import minimist from "minimist";
 import readline from "readline";
 import sqlite3 from "sqlite3";
+// import prompt from "enquirer";
+import Enquirer from "enquirer";
 
 function regiestMemo(texts) {
   const db = new sqlite3.Database("./memo.sqlite3");
@@ -12,26 +14,49 @@ function regiestMemo(texts) {
   db.close();
 }
 
-const argv = minimist(process.argv.slice(2));
-//console.log(argv);
+// const argv = minimist(process.argv.slice(2));
+// //console.log(argv);
 
-// メモをDBへ登録
-const text = readline.createInterface({
-  input: process.stdin
-});
-var texts = [];
-text.on("line", (text) => {
-  texts.push(text);
-});
-text.on("close", () => {
-  // regiestMemo(texts);
-});
+// // メモをDBへ登録
+// const text = readline.createInterface({
+//   input: process.stdin
+// });
+// var texts = [];
+// text.on("line", (text) => {
+//   texts.push(text);
+// });
+// text.on("close", () => {
+//   // regiestMemo(texts);
+// });
 
-// メモの最初の行のみの一覧を表示
+// // メモの最初の行のみの一覧を表示
+// const db = new sqlite3.Database("./memo.sqlite3");
+// db.each("SELECT rowid AS id, memo FROM memos", (err, row) => {
+//   var memo = row.memo;
+//   console.log(memo);
+//   var line = memo.split(/\r\n/);
+//   // console.log(line[0]);
+// });
+// db.close();
+
+// メモの最初の行を参照
+async function ref(lines) {
+  const prompt = new Enquirer.Select({
+    name: "memo",
+    message: "Choose a note you want to see",
+    choices: lines
+  });
+
+  prompt
+    .run()
+    .then((answer) => console.log("Answer:", answer))
+    .catch(console.error);
+}
+
+var lines = [];
 const db = new sqlite3.Database("./memo.sqlite3");
 db.each("SELECT rowid AS id, memo FROM memos", (err, row) => {
-  var memo = row.memo;
-  var line = memo.split(/\r\n/);
-  console.log(line[0]);
+  lines.push(row.memo.split(/\r\n/)[0]);
+  ref(lines);
 });
 db.close();
