0d0fa2b606e4ffdc15d95b2992c625c938679482
goruchan <ppcfr481@yahoo.co.jp>
2023-01-30 21:37:37 +0900

    update 再代入のない変数をconst化

--- 04.class/bin/run.js
+++ 04.class/bin/run.js
@@ -3,5 +3,5 @@ import minimist from "minimist";
 import Input from "../lib/input.js";
 import Controller from "../lib/contoller.js";
 
-let input = new Input(minimist(process.argv.slice(2)));
+const input = new Input(minimist(process.argv.slice(2)));
 new Controller(input).commandSelect();
