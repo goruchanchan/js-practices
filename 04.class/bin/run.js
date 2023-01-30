5f41a36a800321c9a3303b1e85940d91806f0371
goruchan <ppcfr481@yahoo.co.jp>
2023-01-30 21:43:58 +0900

    update メソッド名更新

--- 04.class/bin/run.js
+++ 04.class/bin/run.js
@@ -4,4 +4,4 @@ import Input from "../lib/input.js";
 import Controller from "../lib/contoller.js";
 
 const input = new Input(minimist(process.argv.slice(2)));
-new Controller(input).commandSelect();
+new Controller(input).selectCommand();
