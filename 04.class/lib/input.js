f93317381d618d9b0c9aee4faf7544d2aef01116
goruchan <ppcfr481@yahoo.co.jp>
2023-01-31 22:48:52 +0900

    clean ドット表記法へ変更

--- 04.class/lib/input.js
+++ 04.class/lib/input.js
@@ -4,11 +4,11 @@ class Input {
   constructor(option) {
     this.option = null;
     this.contents = [];
-    if (option["r"]) {
+    if (option.r) {
       this.option = "r";
-    } else if (option["l"]) {
+    } else if (option.l) {
       this.option = "l";
-    } else if (option["d"]) {
+    } else if (option.d) {
       this.option = "d";
     } else {
       this.readConsole();
