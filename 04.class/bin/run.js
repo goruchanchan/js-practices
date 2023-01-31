914b5f53acde1dd48e2b5aa79e5603928f9ba98c
goruchan <ppcfr481@yahoo.co.jp>
2023-01-31 22:55:08 +0900

    clean ファイル名typo修正

--- 04.class/bin/run.js
+++ 04.class/bin/run.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import minimist from "minimist";
 import Input from "../lib/input.js";
-import Controller from "../lib/contoller.js";
+import Controller from "../lib/controller.js";
 
 const input = new Input(minimist(process.argv.slice(2)));
 new Controller(input).selectCommand();
